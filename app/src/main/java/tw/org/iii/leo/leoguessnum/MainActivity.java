b327ef8ae28c59fcc7a4020d90fb832f8465fccb
Leo <icpc0928@gmail.com>
2020-03-17 01:38:21 +0800

    20200317 v2 btnListener

--- app/src/main/java/tw/org/iii/leo/leoguessnum/MainActivity.java
+++ app/src/main/java/tw/org/iii/leo/leoguessnum/MainActivity.java
@@ -3,12 +3,48 @@ package tw.org.iii.leo.leoguessnum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;
+    private TextView num1,num2,num3,num4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        TextView  配置
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+        num3 = findViewById(R.id.num3);
+        num4 = findViewById(R.id.num4);
+//        0-9按鈕 配置
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btn0 = findViewById(R.id.btn0);
+
+
+
+
+    }
+
+
+//    多按鈕監聽 0-9按鈕
+    public void btnNum(View view) {
+        Button whichBtn = (Button)view;
+        Log.v("leo",whichBtn.getText().toString());
+
     }
 }
